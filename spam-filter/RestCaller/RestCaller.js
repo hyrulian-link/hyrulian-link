b15e448e48a10026fca654ec59e535845e2883b4
Matt Graves <graves555@hotmail.com>
2019-11-19 16:09:01 -0600

    deleting junk when addin loads

--- spam-filter/RestCaller/RestCaller.js
+++ spam-filter/RestCaller/RestCaller.js
@@ -6,9 +6,6 @@
     var rawToken = '';
     var parsedToken = '';
 
-    var getItemSpinnerElement = null;
-    var getItemSpinner = null;
-
     var Queue = function () {
         var previous = new $.Deferred().resolve();
 
@@ -23,66 +20,16 @@
     // The Office initialize function must be run each time a new page is loaded
     Office.initialize = function (reason) {
         $(document).ready(function () {
-            app.initialize();
-            var PivotElements = document.querySelectorAll('.ms-Pivot');
-            for (var i = 0; i < PivotElements.length; i++) {
-                new fabric['Pivot'](PivotElements[i]);
-            }
-
-            var ToggleElements = document.querySelectorAll('.ms-Toggle');
-            for (var i = 0; i < ToggleElements.length; i++) {
-                new fabric['Toggle'](ToggleElements[i]);
-            }
-
-            getItemSpinnerElement = document.querySelector('.get-item-spinner');
-            getItemSpinner = new fabric['Spinner'](getItemSpinnerElement);
-            getItemSpinner.stop();
-
-            var DropdownHTMLElements = document.querySelectorAll('.ms-Dropdown');
-            for (var i = 0; i < DropdownHTMLElements.length; ++i) {
-                var Dropdown = new fabric['Dropdown'](DropdownHTMLElements[i]);
-            }
-
-            $('#parse-token-toggle').click(function () {
-                loadToken($('#parse-token-toggle').is(':checked'));
-            });
-
-            $('.get-item-button').click(function () {
-                deleteJunk();
-            });
-
             loadRestDetails();
+            deleteJunk();
         });
     };
 
     function loadRestDetails() {
-        $('.hostname').text(Office.context.mailbox.diagnostics.hostName);
-        $('.hostversion').text(Office.context.mailbox.diagnostics.hostVersion);
-        $('.owaview').text(Office.context.mailbox.diagnostics.OWAView);
-
-        var restId = '';
-        if (Office.context.mailbox.diagnostics.hostName !== 'OutlookIOS') {
-            // Loaded in non-mobile context, so ID needs to be converted
-            restId = Office.context.mailbox.convertToRestId(
-                Office.context.mailbox.item.itemId,
-                Office.MailboxEnums.RestVersion.Beta
-            );
-        } else {
-            restId = Office.context.mailbox.item.itemId;
-        }
-
-        // Build the URL to the item
-        //var itemUrl = Office.context.mailbox.restUrl + 
-        var itemUrl = 'https://outlook.office.com' +
-            '/api/beta/me/messages/' + restId;
-
-        $('.resturl-display code').text(itemUrl);
-
         Office.context.mailbox.getCallbackTokenAsync({ isRest: true }, function (result) {
             if (result.status === "succeeded") {
                 rawToken = result.value;
                 loadToken($('#parse-token-toggle').is(':checked'));
-                enableButtons();
             } else {
                 rawToken = 'error';
             }
@@ -108,12 +55,6 @@
     }
 
     function deleteJunk() {
-        toggleGetItemSpinner(true);
-        deleteJunkWithSearch();
-        toggleGetItemSpinner(false);
-    }
-
-    function deleteJunkWithSearch() {
         var junkEmailFolder = getJunkMailFolder();
         var junkUrl = 'https://outlook.office.com/api/beta/me/MailFolders/' + junkEmailFolder +
             '/messages/?$select=Sender,Body&$top=50';
@@ -177,20 +118,4 @@
             headers: { 'Authorization': 'Bearer ' + rawToken }
         });
     }
-
-    function enableButtons() {
-        $('.get-item-button').removeClass('is-disabled');
-        $('.update-item-button').removeClass('is-disabled');
-    }
-
-    function toggleGetItemSpinner(showSpinner) {
-        if (showSpinner) {
-            getItemSpinner.start();
-            getItemSpinnerElement.style.display = "block";
-        } else {
-            getItemSpinner.stop();
-            getItemSpinnerElement.style.display = "none";
-        }
-    }
-
 })();
